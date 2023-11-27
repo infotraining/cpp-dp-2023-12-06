7b1fa3b2ea10ef081c38c9c98a4830f8d40900f0
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-27 12:12:46 +0100

    Initial commit

---
+++ Creational/Prototype.Exercise/shape.hpp
@@ -0,0 +1,42 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include "point.hpp"
+
+namespace Drawing
+{
+    class Shape
+    {
+    public:
+        virtual ~Shape() = default;
+        virtual void move(int x, int y) = 0;
+        virtual void draw() const = 0;
+    };
+
+    class ShapeBase : public Shape
+    {
+        Point coord_; // composition
+    public:
+        Point coord() const
+        {
+            return coord_;
+        }
+
+        void set_coord(const Point& pt)
+        {
+            coord_ = pt;
+        }
+
+        ShapeBase(int x = 0, int y = 0)
+            : coord_{x, y}
+        {
+        }
+
+        void move(int dx, int dy) override
+        {
+            coord_.translate(dx, dy);
+        }
+    };
+}
+
+#endif // SHAPE_HPP
