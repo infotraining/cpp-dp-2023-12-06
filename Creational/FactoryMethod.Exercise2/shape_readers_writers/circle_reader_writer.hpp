4d76ff8e88af748b459bb93ad4b3cd1f8644441c
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-12-06 16:10:13 +0100

    ShapeFactories

--- Creational/FactoryMethod.Exercise2/shape_readers_writers/circle_reader_writer.hpp
+++ Creational/FactoryMethod.Exercise2/shape_readers_writers/circle_reader_writer.hpp
@@ -1,15 +1,31 @@
 #ifndef CIRCLEREADERWRITER_H
 #define CIRCLEREADERWRITER_H
 
+#include "../circle.hpp"
 #include "shape_reader_writer.hpp"
 
 namespace Drawing
 {
     namespace IO
     {
-        // TODO
         class CircleReaderWriter : public ShapeReaderWriter
         {
+        public:
+            void read(Shape& shp, std::istream& in) override
+            {
+                Circle& circle = dynamic_cast<Circle&>(shp);
+                Point pt;
+                int r;
+                in >> pt >> r;
+                circle.set_coord(pt);
+                circle.set_radius(r);
+            }
+
+            void write(const Shape& shp, std::ostream& out) override
+            {
+                const Circle& circle = dynamic_cast<const Circle&>(shp);
+                out << Circle::id << " " << circle.coord().x << " " << circle.coord().y << " " << circle.radius() << "\n";
+            }
         };
     }
 }
