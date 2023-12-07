6e0c2d96e3d224a37913fbb7e392659a0ace556d
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-12-07 15:24:19 +0100

    Decorator with Builder

--- Structural/Decorator.Exercise/main.cpp
+++ Structural/Decorator.Exercise/main.cpp
@@ -1,6 +1,7 @@
 #include "starbugs_coffee.hpp"
 
 #include <memory>
+#include <cassert>
 
 void client(std::unique_ptr<Coffee> coffee)
 {
@@ -8,10 +9,64 @@ void client(std::unique_ptr<Coffee> coffee)
     coffee->prepare();
 }
 
+template <typename HeadDecorator, typename... TailDecorators>
+struct DecorateWith
+{
+    static std::unique_ptr<Coffee> make_decorator(std::unique_ptr<Coffee> component)
+    {
+        auto decorator_component = std::make_unique<HeadDecorator>(std::move(component));
+
+        return DecorateWith<TailDecorators...>::make_decorator(std::move(decorator_component));
+    }
+};
+
+template <typename HeadDecorator>
+struct DecorateWith<HeadDecorator>
+{
+    static std::unique_ptr<Coffee> make_decorator(std::unique_ptr<Coffee> component)
+    {
+        return std::make_unique<HeadDecorator>(std::move(component));
+    }
+};
+
+class CoffeeBuilder
+{
+    std::unique_ptr<Coffee> coffee_;
+
+public:
+    template <typename BaseCoffee>
+    CoffeeBuilder& create_base()
+    {
+        coffee_ = std::make_unique<BaseCoffee>();
+
+        return *this;
+    }
+
+    template <typename... Condiments>
+    CoffeeBuilder& add()
+    {
+        assert(coffee_);
+
+        coffee_ = DecorateWith<Condiments...>::make_decorator(std::move(coffee_));
+
+        return *this;
+    }
+
+    std::unique_ptr<Coffee> get_coffee()
+    {
+        return std::move(coffee_);
+    }
+};
+
 int main()
 {
     std::unique_ptr<Coffee> cf = std::make_unique<WhippedCream>(std::make_unique<Whisky>(std::make_unique<ExtraEspresso>(std::make_unique<Espresso>())));
     client(std::move(cf));
 
-    
+    CoffeeBuilder cb;
+    cb.create_base<Espresso>()
+        .add<ExtraEspresso>()
+        .add<Whisky, Whisky, Whisky, WhippedCream>();
+
+    client(cb.get_coffee());
 }
