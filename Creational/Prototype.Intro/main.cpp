f18658432d8df77f4b8181c63b5ec1fdc9b5dc69
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-12-07 11:17:48 +0100

    Prototype CRTP

--- Creational/Prototype.Intro/main.cpp
+++ Creational/Prototype.Intro/main.cpp
@@ -8,38 +8,49 @@ class Engine
 public:
     virtual void start() = 0;
     virtual void stop() = 0;
+    virtual std::unique_ptr<Engine> clone() const = 0; // prototype - virtual wrapper for copy constructor
     virtual ~Engine() = default;
 };
 
-class Diesel : public Engine
+template <typename TEngine, typename TEngineBase = Engine>
+class CloneableEngine : public TEngineBase
 {
 public:
-    virtual void start() override
+    std::unique_ptr<Engine> clone() const override
+    {
+        return std::make_unique<TEngine>(static_cast<const TEngine&>(*this)); 
+    }
+};
+
+class Diesel : public CloneableEngine<Diesel>  // CRTP 
+{
+public:
+    void start() override
     {
         std::cout << "Diesel starts\n";
     }
 
-    virtual void stop() override
+    void stop() override
     {
         std::cout << "Diesel stops\n";
     }
 };
 
-class TDI : public Diesel
+class TDI : public CloneableEngine<TDI, Diesel>
 {
 public:
-    virtual void start() override
+    void start() override
     {
         std::cout << "TDI starts\n";
     }
 
-    virtual void stop() override
+    void stop() override
     {
         std::cout << "TDI stops\n";
     }
 };
 
-class Hybrid : public Engine
+class Hybrid : public CloneableEngine<Hybrid>
 {
 public:
     virtual void start() override
@@ -63,6 +74,20 @@ public:
     {
     }
 
+    Car(const Car& source) : engine_{source.engine_->clone()}
+    {
+    }
+
+    Car& operator=(const Car& source)
+    {
+        if (this != &source)
+        {
+            engine_ = source.engine_->clone();
+        }
+
+        return *this;
+    }
+
     void drive(int km)
     {
         engine_->start();
@@ -73,8 +98,11 @@ public:
 
 int main()
 {
-    Car c1{std::make_unique<Hybrid>()};
+    Car c1{std::make_unique<TDI>()};
     c1.drive(100);
 
     std::cout << "\n";
+
+    Car c2 = c1;
+    c2.drive(300);
 }
